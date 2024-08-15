6b0bcce29004801858e6912428667a5aebd0e531
Jeff Balbalosa <cyjeffb@yahoo.com>
2024-08-15 09:58:32 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Welcome to the Adventure Game!");
+Console.WriteLine("Enter your character's name: ");
+string playername = Console.ReadLine();
+
+Console.WriteLine("Choose your charcter type (Warrioe, Wizard, Archer)");
+string charcterType = Console.ReadLine();   
+
+
+Console.ReadKey();
